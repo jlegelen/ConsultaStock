7ba43eb6ac719370cfb6ebd237df268968cd0cc3
NB-L460-JP\JPLegelen <juanplegelen@gmail.com>
2021-04-13 21:07:42 -0300

    first commit

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,8 @@
 const env = {
-  server_url: "http://localhost:5000"
+  //server_url: "http://localhost:5000"
   //server_url: "http://192.168.2.114:5000"
-  //server_url: "https://33387c23.ngrok.io"
+  //server_url: "http://aac5d021adf8.ngrok.io"
+  server_url: "https://app-consulta-stock-bo.herokuapp.com"
   };
 
 module.exports = env;
